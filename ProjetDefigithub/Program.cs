ff7db55b0ac3fe17a63b8b1fe0aea6e77424168c
rinyo1833097 <rinyo1833097@etu.cegepjonquiere.ca>
2018-09-17 13:53:14 -0400

    defi 1

--- ProjetDefigithub/Program.cs
+++ ProjetDefigithub/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace ProjetDefigithub
+namespace Defi
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string texte = "Mb`qsph`d`ftu`m(gvo";
+            int cara = 0;
+            string texte2 = "";
+
+
+            for (int i = 0; i < texte.Length; i++)
+            {
+
+
+                cara = Convert.ToInt32(texte[i]);
+
+
+                cara = cara - 1;
+                texte2 = texte2 + Convert.ToChar(cara);
+
+            }
+            Console.WriteLine(texte2);
+            // c est dur olala
         }
     }
 }
